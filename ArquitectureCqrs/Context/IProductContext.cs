733d34ca545fc8a7fcedf5d435d22d70563bc183
Mario Villanueva <mario.villanueva@sermaluc.cl>
2021-08-16 21:07:24 -0400

    mod: implementacion

--- ArquitectureCqrs/Context/IProductContext.cs
+++ ArquitectureCqrs/Context/IProductContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PatronCqrs.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace PatronCqrs.Context
 {
     public interface IProductContext
     {
+        Task<int> Add(Product product);
+        Task<bool> Remove(int id);
+        Task<bool> Update(Product product);
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(int id);
     }
+
 }
---
+++ ArquitectureCqrs/Context/ProductContext.cs
@@ -0,0 +1,41 @@
+﻿using PatronCqrs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatronCqrs.Context
+{
+    public class ProductContext : IProductContext
+    {
+
+        readonly string ConnectionString;
+        public ProductContext(string connectionString) => ConnectionString = connectionString;
+
+
+        public Task<int> Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
