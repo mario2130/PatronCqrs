a75442b0cce1d63dd71e0a1957b622982cb257bf
Mario Villanueva <mario.villanueva@sermaluc.cl>
2021-08-16 20:57:13 -0400

    mod: context

---
+++ ArquitectureCqrs/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatronCqrs.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
+
+}
